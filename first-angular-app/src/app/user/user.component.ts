711ddc4f3d7e5a2f8fb5a1d74ad993ba6c4ad105
Varsha Gopal <47279135+varshagopal@users.noreply.github.com>
2025-08-01 13:24:40 +0530

    created header and user components

---
+++ first-angular-app/src/app/user/user.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user',
+  imports: [],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css',
+})
+export class UserComponent {}
